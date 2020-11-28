83e325ce0c7db089c828467e2f42b1e6adecd75c
victor840 <victor.ugoanusi@gmail.com>
2020-11-28 13:07:55 -0500

    Updated Vendingmachine class

--- src/main/java/com/codedifferently/walkthrough/vendingmachine/VendingMachine.java
+++ src/main/java/com/codedifferently/walkthrough/vendingmachine/VendingMachine.java
@@ -1,15 +1,73 @@
 package com.codedifferently.walkthrough.vendingmachine;
 import com.codedifferently.walkthrough.vendingmachine.Menu.Menu;
+import com.codedifferently.walkthrough.vendingmachine.inventory.*;
+import org.apache.commons.io.IOUtils;
+
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class VendingMachine {
     private Scanner scan;
+    private Map<String, Product> inventory;
     public VendingMachine() {
         this.scan = new Scanner(System.in);
+        this.inventory = new HashMap<>();
     }
+
+    private void init(){
+        try {
+            String output = readRawDataToString();
+            String[] products = output.split("\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Product makeProductFromString(String[] productData){
+        Product product = null;
+        String type = productData[3];
+        String name = productData[1];
+        Double price = Double.valueOf(productData[2]);
+        switch(type){
+            case "Candy":
+                product = new Candy(name, price);
+                break;
+            case "Beverage":
+                product = new Beverage(name, price);
+                break;
+            case "Chips":
+                product = new Chips(name, price);
+                break;
+            case "Gum":
+                product = new Gum(name, price);
+                break;
+            default:
+                break;
+        }
+        return product;
+    }
+    public void stockVendingMachine(String[] products){
+        for (String product : products){
+            String[] productData = product.split("\\|");
+            String key = productData[0];
+            Product item = makeProductFromString(productData);
+            inventory.put(key, item);
+        }
+
+    }
+
+    private String readRawDataToString() throws Exception{
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("inventory.txt"));
+        return result;
+    }
+
     public void start(){
+        init();
         System.out.println("Welcome to Mazi Vee's Vending Machine");
         boolean flag = true;
         ArrayList<String> options = new ArrayList<>();
@@ -34,10 +92,13 @@ public class VendingMachine {
         }
         scan.close();
     }
-    public static void main(String[] args) {
+
+    public Map<String, Product> getInventory(){
+        return this.inventory;
+    }
+    public static void main(String[] args){
 
         VendingMachine vendingMachine = new VendingMachine();
-
         vendingMachine.start();
 
 
--- src/test/java/com/codedifferently/walkthrough/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/codedifferently/walkthrough/vendingmachine/VendingMachineTest.java
@@ -1,11 +1,41 @@
 package com.codedifferently.walkthrough.vendingmachine;
 
+import com.codedifferently.walkthrough.vendingmachine.inventory.Beverage;
+import com.codedifferently.walkthrough.vendingmachine.inventory.Candy;
+import com.codedifferently.walkthrough.vendingmachine.inventory.Product;
 import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
 
 public class VendingMachineTest {
     private final static Logger logger = Logger.getLogger(VendingMachineTest.class);
 
-    public static void main(String[] args) {
-        logger.info("Start");
+    @Test
+    public void stockVendingMachineTest(){
+        String[] sample = {"A1|Sour Patch Kids|1.25|Candy"};
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.stockVendingMachine(sample);
+
+        Product expectedCandy = new Candy("Sour Patch Kids", 1.25);
+        Map<String, Product> inventory = vendingMachine.getInventory();
+        Product actualCandy = inventory.get("A1");
+
+        Assert.assertEquals(expectedCandy.getName(), actualCandy.getName());
+    }
+
+    @Test
+    public void stockVendingMachineTest2(){
+        String[] sample = {"A2|Coke|1.00|Beverage"};
+        VendingMachine vendingMachine = new VendingMachine();
+        vendingMachine.stockVendingMachine(sample);
+
+        Product expectedBeverage = new Beverage("Coke", 1.00);
+        Map<String, Product> inventory = vendingMachine.getInventory();
+        Product actualBeverage = inventory.get("A2");
+
+        Assert.assertEquals(expectedBeverage.getName(), actualBeverage.getName());
     }
 }
+
